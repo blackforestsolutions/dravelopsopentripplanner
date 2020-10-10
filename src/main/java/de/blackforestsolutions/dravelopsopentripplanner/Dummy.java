65aa3d6229b318d1406d52c0d9193e3b1726967e
hennihaus <jan-hendrik.hausner@outlook.com>
2020-10-10 12:40:21 +0200

    initial commit

---
+++ src/main/java/de/blackforestsolutions/dravelopsopentripplanner/Dummy.java
@@ -0,0 +1,10 @@
+package de.blackforestsolutions.dravelopsopentripplanner;
+
+public class Dummy {
+
+    public static final int TWO = 2;
+
+    public int returnTwo() {
+        return TWO;
+    }
+}
