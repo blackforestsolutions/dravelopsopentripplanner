8266797bf709add053222e9820039551ef692e46
hennihaus <jan-hendrik.hausner@outlook.com>
2021-01-23 13:59:42 +0100

    reduced ram for development through a smaller map. removed docker push command as it is a submodule of dravelopsdeployment

--- build-graph.sh
+++ build-graph.sh
@@ -1,10 +1,9 @@
 BASEDIR=$(dirname "$0")
 #This script must be called with "sh build-graph.sh $1 bw"
 PROJECT_NAME=bw
-if [ $# != 0 ]
-then
-    PROJECT_NAME="$1"
-    shift 1
+if [ $# != 0 ]; then
+  PROJECT_NAME="$1"
+  shift 1
 fi
 source $BASEDIR/projects/$PROJECT_NAME/config.sh
 
@@ -18,20 +17,20 @@ mkdir graphs
 #download gtfs
 printf "\nStarting download Gtfs:"
 for GTFS_SUPPLIER in ${!GTFS_SUPPLIERS[@]}; do
-    GTFS_SUPPLY_LINK=${GTFS_SUPPLIERS[${GTFS_SUPPLIER}]}
-    cd graphs
-    mkdir -p $DEPLOY_FOLDER
-    cd $DEPLOY_FOLDER
-    printf "\nStarting download $GTFS_SUPPLIER from link $GTFS_SUPPLY_LINK \n"
-    curl $GTFS_SUPPLY_LINK -O /graphs/$DEPLOY_FOLDER
-    cd ..
-    cd ..
-    printf "\nFinished download. $GTFS_SUPPLIER was saved in /graphs/$DEPLOY_FOLDER"
+  GTFS_SUPPLY_LINK=${GTFS_SUPPLIERS[${GTFS_SUPPLIER}]}
+  cd graphs
+  mkdir -p $DEPLOY_FOLDER
+  cd $DEPLOY_FOLDER
+  printf "\nStarting download $GTFS_SUPPLIER from link $GTFS_SUPPLY_LINK \n"
+  curl $GTFS_SUPPLY_LINK -O /graphs/$DEPLOY_FOLDER
+  cd ..
+  cd ..
+  printf "\nFinished download. $GTFS_SUPPLIER was saved in /graphs/$DEPLOY_FOLDER"
 done
 printf "\nSuccessfully downloaded Gtfs"
 
 #download map material
-printf "\nStarting download map material Continent: $CONTINENT, Country: $COUNTRY, Region: $REGION"
+printf "\nStarting download map material from $OSM_MAP"
 cd graphs
 mkdir -p $DEPLOY_FOLDER
 cd $DEPLOY_FOLDER
@@ -47,9 +46,5 @@ printf "\nStart Build Graph with otp"
 java $JAVA_OPTIONS -jar otp-$OTP_VERSION-shaded.jar --build graphs/$DEPLOY_FOLDER
 printf "\nBuild Graph with otp was successful"
 
-# build, start, tag and push container
+# build and start container
 docker-compose up -d
-
-docker tag blackforestsolutions/dravelopsopentripplanner:latest
-
-docker push blackforestsolutions/dravelopsopentripplanner:latest
